fda709ba77c0327a44ce2d8ae8bd27c54c2961ea
Ubuntu <ubuntu@ip-172-31-40-64.us-west-2.compute.internal>
2013-07-12 19:42:04 +0000

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var content;
+    fs.readFile('Index.html', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+    });
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
