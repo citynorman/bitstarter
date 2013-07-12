c8a947ca350a323eebe0ce4d087b1d9c5a46bf74
Ubuntu <ubuntu@ip-172-31-40-64.us-west-2.compute.internal>
2013-07-12 20:01:04 +0000

    buffer index.html

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
-var content;
-    fs.readFile('Index.html', function read(err, data) {
-        if (err) {
-            throw err;
-        }
-        content = data;
-    });
+var fs = require('fs');
+var text = fs.readFileSync('index.html');
+var buffer = new Buffer(text);
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
